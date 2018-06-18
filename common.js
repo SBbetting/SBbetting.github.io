a9e52d85790abca5f8b66b96ea2bf82a9dad953c
yukairex <wyktju@gmail.com>
2018-06-17 20:40:34 -0400

    upgrade to mainnet

--- common.js
+++ common.js
@@ -1,11 +1,15 @@
-            //use test net
-            const SB_CONTRACT_ADDRESSES = "0x6dda976cfb8db0d85cd9b5dbe28139e148ebcef9";
-            const BET_CONTRACT_ADDRESSES = "0xce82450ab55d27cb2c224e888e905c372c9ddb07";
-            const explorerTx = "https://rinkeby.etherscan.io/tx/";
-            const explorer = "https://rinkeby.etherscan.io/address/";
-            const networkId = 4;// rinkeby
+            // // //use test net
+            // const SB_CONTRACT_ADDRESSES = "0x6dda976cfb8db0d85cd9b5dbe28139e148ebcef9";
+            // const BET_CONTRACT_ADDRESSES = "0xce82450ab55d27cb2c224e888e905c372c9ddb07";
+            // const explorerTx = "https://rinkeby.etherscan.io/tx/";
+            // const explorer = "https://rinkeby.etherscan.io/address/";
+            // const networkId = 4;// rinkeby
 
 
-            // const CONTRACT_ADDRESSES = "0xfea71f436ec963507c26f424d93168e4285cd822";
-            // const explorerTx = "https://etherscan.io/tx/";
-            // const explorer = "https://etherscan.io/address/"
+
+
+            const SB_CONTRACT_ADDRESSES = "0x9af86e5724298fdbbeed1808062784a1ae76767b";
+            const BET_CONTRACT_ADDRESSES = "0x21d2b3a04fa3a9da23ff85b7a3b66f46a067bf4c";
+            const explorerTx = "https://etherscan.io/tx/";
+            const explorer = "https://etherscan.io/address/"
+            const networkId = 1// main
