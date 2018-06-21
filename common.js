32a1d91941200814a9b3d359ace14fe3a34f23cd
yukairex <wyktju@gmail.com>
2018-06-20 23:44:30 -0400

    update matchup

--- common.js
+++ common.js
@@ -7,7 +7,6 @@
 
 
 
-
             const SB_CONTRACT_ADDRESSES = "0x9af86e5724298fdbbeed1808062784a1ae76767b";
             const BET_CONTRACT_ADDRESSES = "0xa666b081583dbe8018af7c7c6e8bb6954c8984d2";
             const explorerTx = "https://etherscan.io/tx/";
